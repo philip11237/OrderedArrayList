810e1121f1cdd6f82b96c4135c39f77bd66587bb
philip von mueffling <philip.vm3@gmail.com>
2020-12-13 22:42:53 -0500

    add again

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -12,10 +12,5 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     }
   super.add(index, element);
   }
-  	public T set(int index, T element){
-      if (element==null){
-        throw new IllegalArgumentException();
-      }
-    return super.set(index, element);
-    }
+  	
 }
