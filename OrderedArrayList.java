34a3dfbe74ba2d13e9dcc698e20140178a2041c8
philip von mueffling <philip.vm3@gmail.com>
2020-12-13 23:18:20 -0500

    add1

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,27 @@
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T>{
+  public OrderedArrayList(){
+  super();
+  }
+  public OrderedArrayList(int startingCapacity){
 
+    super(startingCapacity);
+  }
+
+  public boolean add(T e){
+    int i=0;
+    while (i<this.size()&&(this.get(i).compareto(e) < 0)){
+      i++;
+    }
+    super.add(i,e);
+    return true;
+  }
+  public void add(int index, T element){
+
+  super.add(index, element);
+  }
+    public T set(int index, T element){
+
+    return super.set(index, element);
+    }
+  }
 }
