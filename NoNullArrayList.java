fc2a050f8da8076d1b2d47d63188748bcd01b6e5
philip von mueffling <philip.vm3@gmail.com>
2020-12-13 22:49:18 -0500

    nonullarraylist

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,13 @@
 import java.lang.IllegalArgumentException;
 public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+  super();
+}
+  public NoNullArrayList(int startingCapacity){
+
+    super(startingCapacity);
+  }
+
   public boolean add(T e){
     if (e==null){
       throw new IllegalArgumentException();
